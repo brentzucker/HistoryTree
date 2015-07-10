90167e0fe4f59d314dda700b13e44050b37bbcb2
brentzucker <brentzucker@gmail.com>
2015-07-09 21:56:23 -0400

    consumes a url and shows the history item and corresponding visit items for it

--- history.js
+++ history.js
@@ -1,9 +1,8 @@
-function getHistory(){
-	query = '';
+function getHistory(url){
+	query = url;
 	chrome.history.search({text: query}, function(array_of_history_item_results){
 
-	var ul = document.getElementById('list');
-		console.log(array_of_history_item_results);
+		var ul = document.getElementById('list');
 
 		for(var i=0; i<array_of_history_item_results.length; i++){
 			var history_item = array_of_history_item_results[i];
@@ -12,16 +11,38 @@ function getHistory(){
 			var time = millisecondsToDateTimeString(history_item.lastVisitTime);
 			var name = history_item.title;
 			var id = history_item.id;
-			var link_text = time + '  -  ' + name + '  -  ' + id;
+			var link_text = time + '  -  ' + link + '  -  ' + id;
 
 			var a = document.createElement('a');
 			a.innerHTML = link_text;
 			a.href = link;
+			a.target = '_blank';
 
 		 	var li = document.createElement('li');
 		 	li.appendChild(a);
 		 	ul.appendChild(li);
-		}		
+
+		 	getVisits(history_item);
+		}	
+	});
+};
+
+function getVisits(url_input){
+	var ul = document.getElementById('list');
+
+	chrome.history.getVisits({url:url_input}, function(array_of_visit_item_results){
+
+		for(var j=0; j<array_of_visit_item_results.length; j++){
+			visit_item = array_of_visit_item_results[j];
+
+			for (var property in visit_item){
+				if(visit_item.hasOwnProperty(property)){
+					var li = document.createElement('li');
+					li.appendChild(document.createTextNode(property + ' : ' + visit_item[property]));
+					ul.appendChild(li);
+				}
+			}
+		}
 	});
 };
 
@@ -33,7 +54,12 @@ function millisecondsToDateTimeString(milliseconds){
 
 document.addEventListener('DOMContentLoaded', function() {
 	console.log('Hello World');
-	getHistory();
+	
+	var url = '';
+
+	getHistory(url);
+
+	getVisits(url);
+
 	console.log('done');
 });
-
