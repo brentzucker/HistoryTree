914bc3640f8bdcd80e07e95c2245bcf2732f0e63
Bogdan Vitoc <bogdanvitoc@gmail.com>
2015-07-11 15:00:15 -0400

    Implemented recursion and separated VisitItems from HistoryItems

--- history.js
+++ history.js
@@ -1,93 +1,62 @@
-function getHistory(url){
-	query = url;
-	chrome.history.search({text: query}, function(array_of_history_item_results){
+//Gets History Items
+function getHistory(query){
+	chrome.history.search({text: query, startTime: 1436208042000, maxResults: 10000}, function(history_items){
+		
+		//Place in DOM
+		historyItemsToDOM(history_items);
 
-		var ul = document.getElementById('list');
-
-		for(var i=0; i<array_of_history_item_results.length; i++){
-			var history_item = array_of_history_item_results[i];
-
-			var link_text = historyItemToString(history_item);
-
-			var a = buildLink(link_text, history_item.url);
-
-		 	var li = document.createElement('li');
-		 	li.appendChild(a);
-		 	ul.appendChild(li);
-
-		 	//Gets visits 
-		 	getVisits(history_item.url);
-		}
-		//How can I return from here?
-		return array_of_history_item_results;	
+		//Gets visits
+	 	var start_time = 1436208042000;
+	 	getVisits(history_items, start_time);
 	});
 };
 
-var results = [];
-
-function getHistoryItems(url, start_time) {
+//Get Visit Items
+function getVisits(array_of_history_item_results, start_time){
 	
+	//Build urls array
+	var urls = [];
+	for (var i = array_of_history_item_results.length - 1; i >= 0; i--) {
+
+		var history_item = array_of_history_item_results[i];
+
+		if (typeof history_item !== 'undefined') {
+			urls.push(history_item.url);
+		}
+	};
+
 	var all_visited_items = [];
-	chrome.history.search({text: url, startTime : start_time}, function(history_items) {
-		
-		//loop through all history items, get each array for visited items
-		for (var i = 0; i < history_items.length; i++) {
 
-			var url_to_retrieve = history_items[i].url;
+	//Function to recurse for each url
+	function recurse(idx) {
+		chrome.history.getVisits({ url:urls[idx] }, function(visit_items){
 
-			(function () {
+			for (var j = 0; j < visit_items.length; j++) {
 
-				chrome.history.getVisits({ url : url_to_retrieve }, function(visit_items) {
+				var visit_item = visit_items[j];
 
-					//append all visit_items to all_visited_items
-					//all_visited_items.push(visit_items);
-					for (var j = 0; j < visit_items.length; j++) {
+				if (typeof visit_item !== 'undefined') {
 
-						//If the visit occurred after the given start date and is defined
-
-						if (visit_items[i] !== undefined) {
-
-							if (start_time > visit_items[i].visitTime) {
-
-								all_visited_items.push(visit_items[i]);
-								console.log((new Date(start_time)) + '  <?  ' + (new Date(visit_items[i].visitTime)) );
-							}
-							else
-							{
-								//console.log(visit_items[i]);
-							}
-						}
+					//Visits after start_time
+					if (start_time > visit_item.visitTime) {
+						all_visited_items.push(visit_item);
 					}
-				});
-			}) ();
-		}
 
-		console.log('All visited items');
-		console.log(all_visited_items);
-	});
-}
-
-function getVisits(url_input){
-	var ul = document.getElementById('list');
-
-	chrome.history.getVisits({url:url_input}, function(array_of_visit_item_results){
-
-		for(var j=0; j<array_of_visit_item_results.length; j++){
-			visit_item = array_of_visit_item_results[j];
-			//console.log(visit_item);
-
-			for (var property in visit_item){
-				if(visit_item.hasOwnProperty(property)){
-
-					var output_string = property + ' : ' + visit_item[property];
-
-					var li = document.createElement('li');
-					li.appendChild(document.createTextNode(output_string));
-					ul.appendChild(li);
 				}
-			}			
-		}
-	});
+			}
+
+			//There's more to recurse
+			if (idx + 1 < urls.length) {
+				recurse(idx + 1);
+			} 
+
+			//ALL DONE! all_visited_items is complete
+			else {
+				console.log(all_visited_items);
+			}
+		});
+	}
+	recurse(0);
 };
 
 /* Helper functions
@@ -102,6 +71,21 @@ function historyItemToString(history_item){
 	var link_text = time + '  -  ' + link + '  -  ' + id;
 	return link_text;
 };
+
+function historyItemsToDOM(history_items) {
+	var ul = document.getElementById('list');
+
+	for (var i=0; i<history_items.length; i++) {
+
+		var history_item = history_items[i];
+		var link_text = historyItemToString(history_item);
+		var a = buildLink(link_text, history_item.url);
+
+	 	var li = document.createElement('li');
+	 	li.appendChild(a);
+	 	ul.appendChild(li);
+	}
+}
 
 function buildLink(link_text, url){
 	var a = document.createElement('a');
@@ -126,9 +110,9 @@ document.addEventListener('DOMContentLoaded', function() {
 	var url = '';
 	var start_time = (new Date('2015-07-07')).getTime();
 
-	var array_of_history_items = getHistory(url);
+	getHistory(url);
 
-	getHistoryItems(url, start_time);
+	// getHistoryItems(url, start_time);
 
 
 	//getVisits(url);
