b04c13ec9600626face5ad5f35c8f29cf247cc93
brentzucker <brentzucker@gmail.com>
2015-07-19 11:54:25 -0400

    visitNode stores URL

--- history.js
+++ history.js
@@ -1,12 +1,12 @@
 //Gets History Items
-function getHistory(query){
-    chrome.history.search({text: query, startTime: 1436208042000, maxResults: 10000}, function(history_items){
+function getHistory(query, start_time) {
+    
+    chrome.history.search({text: query, startTime: start_time, maxResults: 10000}, function(history_items) {
         
         //Place in DOM
         historyItemsToDOM(history_items);
 
         //Gets visits
-        var start_time = 1436208042000;
         getVisits(history_items, start_time);
     });
 }
@@ -16,11 +16,13 @@ function getVisits(array_of_history_item_results, start_time){
     
     //Build urls array
     var urls = [];
+    var url_array = [];
     for (var i = array_of_history_item_results.length - 1; i >= 0; i--) {
 
         var history_item = array_of_history_item_results[i];
 
         if (typeof history_item !== 'undefined') {
+        	url_array[history_item.id] = history_item.url; 
             urls.push(history_item.url);
         }
     }
@@ -51,17 +53,18 @@ function getVisits(array_of_history_item_results, start_time){
 
             //ALL DONE! all_visited_items is complete
             else {
-                treeNodes(all_visited_items);
+                treeNodes(all_visited_items, url_array);
             }
         });
     }
     recurse(0);
 };
 
-function visitNode(visit_item) {
+function visitNode(visit_item, urls) {
 
     //Copy VisitItem attributes
     this.id = visit_item.id;
+    this.url = urls[visit_item.id];
     this.visitId = visit_item.visitId;
     this.visitTime = visit_item.visitTime;
     this.referringVisitId = visit_item.referringVisitId;
@@ -74,75 +77,154 @@ function visitNode(visit_item) {
     }
 }
 
+//Check children & childrens children (Recursively?)
+//Pseudo
+function searchChildren(parentNode, childNode) {
+	
+	for(var i=0; i<parentNode.children.length; i++) {
+		//check referring id
+		if (isParent(parentNode.visitId, childNode.referringVisitId)) {
+			parentNode.addChild(childNode);
+			childNode = undefined;
+		}
 
-function treeNodes(all_visited_items) {
+		// //If the child node has not been matched to a parent
+		// if (childNode !== undefined) {
+		// 	searchChildren(parentNode.children[i], childNode);
+		// }
+	}
+}
+
+//Pseudo
+function isParent(visitId, referringVisitId) {
+	return visitId === referringVisitId
+}
+
+
+function treeNodes(all_visited_items, url_array) {
+	console.log(url_array);
     console.log('All visited items');
     console.log(all_visited_items);
 
     //Tree of visit items
 
-    treeNodes_list = [];
-
-    for (var item in all_visited_items) {
-        console.log(item);
-    }
+    var visitNodes_list = [];
 
     for (var j = 0; j < all_visited_items.length; j++) {
 
         var visit_item = all_visited_items[j];
-        var helper_node = new visitNode(visit_item);
+        var helper_node = new visitNode(visit_item, url_array);
 
-        treeNodes_list.push(helper_node);
-
-        console.log(treeNodes_list);
+        visitNodes_list.push(helper_node);
     }
 
-    console.log('Tree Nodes List');
-    console.log(treeNodes_list);
+    var num_of_root_visit_nodes = (getRootVisitNodes(visitNodes_list)).length;
+
+    console.log("num of root visit nodes " + num_of_root_visit_nodes);
+
+    for (var z = 0; z < num_of_root_visit_nodes; z++) {
+	//while (visitNodes_list.length != num_of_root_visit_nodes) {
+
+    	for (var i = 0; i < visitNodes_list.length; i++) {
+    		visitNode_outer = visitNodes_list[i];
+
+  			for (var j = 0; j < visitNodes_list.length; j++) {
+
+  				//Skip if root node
+  				if (visitNodes_list[j] && visitNodes_list[j].referringVisitId !== 0) {
+  					
+  					visitNode_inner = visitNodes_list[j];
+
+	  				if((visitNode_inner && visitNode_outer) && visitNode_inner.referringVisitId == visitNode_outer.visitId) {
+	  					
+	  					visitNodes_list[i].addChild(visitNodes_list[j]);
+	  					visitNodes_list[j] = undefined;
+	  				}
+
+	  				//If they both are not undefined
+	  				if (visitNodes_list[i] && visitNodes_list[j]) {
+	  					//searchChildren(visitNodes_list[i], visitNodes_list[j]);
+	  				}
+  				}
+  				else {
+  					console.log('skip');
+  				}
+  			}
+    	}
+
+    	console.log('Remove all undefined');
+    	console.log(visitNodes_list);
+
+    	//Removed all undefined elements
+    	for (var i = 0; i < visitNodes_list.length; i++) {
+
+    		if(visitNodes_list[i] === undefined) {
+
+    			console.log('splice');
+    			visitNodes_list.splice(i, 1);
+    		}
+    	}
+    }
+
+    console.log('Visit Nodes List');
+    console.log(visitNodes_list);
 
     //Remove all Roots from all_visited_items
     //A visited_item is a root if its refferingId = 0
 }
 
+function getRootVisitNodes(visitNodes_list) {
+
+	var root_visitNodes = [];
+	for (var i = 0; i < visitNodes_list.length; i++) {
+
+		if(visitNodes_list[i].referringVisitId == 0) {
+			root_visitNodes.push(visitNodes_list[i]);
+		}
+	}
+
+	return root_visitNodes;
+}
+
 function getHistoryItems(url, start_time) {
-    
-    var all_visited_items = [];
-    var visitNodes_list = [];
-    chrome.history.search({text: url, startTime : start_time}, function(history_items) {
-        
-        //loop through all history items, get each array for visited items
-        for (var i = 0; i < history_items.length; i++) {
+	
+	var all_visited_items = [];
+	var visitNodes_list = [];
+	chrome.history.search({text: url, startTime : start_time}, function(history_items) {
+		
+		//loop through all history items, get each array for visited items
+		for (var i = 0; i < history_items.length; i++) {
 
-            var url_to_retrieve = history_items[i].url;
+			var url_to_retrieve = history_items[i].url;
 
-            chrome.history.getVisits({ url : url_to_retrieve }, function(visit_items) {
+			chrome.history.getVisits({ url : url_to_retrieve }, function(visit_items) {
 
-                for (var j = 0; j < visit_items.length; j++) {
-                    
-                    visit_item = visit_items[j]
+				for (var j = 0; j < visit_items.length; j++) {
+					
+					visit_item = visit_items[j]
 
-                    //If the visit occurred after the given start date and is defined
-                    if (visit_item !== undefined) {
+					//If the visit occurred after the given start date and is defined
+					if (visit_item !== undefined) {
 
-                        if (start_time < visit_item.visitTime) {
+						if (start_time < visit_item.visitTime) {
 
-                            //all_visited_items.push(visit_item);
-                            visitNodes_list.push( new visitNode(visit_item) );
-                        }
-                    }
-                }
-            });
-        }
+							//all_visited_items.push(visit_item);
+							visitNodes_list.push( new visitNode(visit_item) );
+						}
+					}
+				}
+			});
+		}
 
-        console.log('All visited items');
-        console.log(all_visited_items);
+		console.log('All visited items');
+		console.log(all_visited_items);
 
-        console.log('Visit Nodes List');
-        console.log(visitNodes_list);
+		console.log('Visit Nodes List');
+		console.log(visitNodes_list);
 
-        //Remove all Roots from all_visited_items
-        //A visited_item is a root if its refferingId = 0
-    });
+		//Remove all Roots from all_visited_items
+		//A visited_item is a root if its refferingId = 0
+	});
 }
 
 /* Helper functions
@@ -150,58 +232,13 @@ function getHistoryItems(url, start_time) {
  */
 
 function historyItemToString(history_item){
-    var link = history_item.url;
-    var time = millisecondsToDateTimeString(history_item.lastVisitTime);
-    var name = history_item.title;
-    var id = history_item.id;
-    var link_text = time + '  -  ' + link + '  -  ' + id;
-    return link_text;
+	var link = history_item.url;
+	var time = millisecondsToDateTimeString(history_item.lastVisitTime);
+	var name = history_item.title;
+	var id = history_item.id;
+	var link_text = time + '  -  ' + link + '  -  ' + id;
+	return link_text;
 };
-
-function buildLink(link_text, url){
-    var a = document.createElement('a');
-    a.innerHTML = link_text;
-    a.href = url;
-    a.target = '_blank';
-    return a;
-};
-
-function millisecondsToDateTimeString(milliseconds){
-    var date_time = new Date(milliseconds);
-    return date_time.toLocaleDateString() + ' ' + date_time.toLocaleTimeString();
-
-};
-
-/* "Main method"
- *
- */
-
-document.addEventListener('DOMContentLoaded', function() {
-    
-    var url = '';
-    var start_time = (new Date('2015-07-07')).getTime();
-
-    //var array_of_history_items = getHistory(url);
-
-    getHistoryItems(url, start_time);
-
-
-    //getVisits(url);
-
-    console.log('done');
-});
-/* Helper functions
- *
- */
-
-function historyItemToString(history_item){
-    var link = history_item.url;
-    var time = millisecondsToDateTimeString(history_item.lastVisitTime);
-    var name = history_item.title;
-    var id = history_item.id;
-    var link_text = time + '  -  ' + link + '  -  ' + id;
-    return link_text;
-}
 
 function historyItemsToDOM(history_items) {
     var ul = document.getElementById('list');
@@ -219,28 +256,32 @@ function historyItemsToDOM(history_items) {
 }
 
 function buildLink(link_text, url){
-    var a = document.createElement('a');
-    a.innerHTML = link_text;
-    a.href = url;
-    a.target = '_blank';
-    return a;
-}
+	var a = document.createElement('a');
+	a.innerHTML = link_text;
+	a.href = url;
+	a.target = '_blank';
+	return a;
+};
 
 function millisecondsToDateTimeString(milliseconds){
-    var date_time = new Date(milliseconds);
-    return date_time.toLocaleDateString() + ' ' + date_time.toLocaleTimeString();
+	var date_time = new Date(milliseconds);
+	return date_time.toLocaleDateString() + ' ' + date_time.toLocaleTimeString();
 
-}
+};
 
 /* "Main method"
  *
  */
 
 document.addEventListener('DOMContentLoaded', function() {
-    
-    var url = '';
-    var start_time = (new Date('2015-07-07')).getTime();
 
-    getHistory(url);
+	$('#header').on('click', function() {
+		chrome.tabs.create({url: 'history.html'});
+	});
+	
+	var url = '';
+    var start_time = (new Date('2015-07-19')).getTime();
+
+    getHistory(url, start_time);
 
 });
